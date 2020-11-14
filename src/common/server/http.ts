871ccf4a469021eb614c26d9a32a11f5a9cee6ec
Karol Szymanowski <szyma.karol@gmail.com>
2020-11-14 23:42:20 +0100

    refactor: improve app logging

--- src/common/server/http.ts
+++ src/common/server/http.ts
@@ -9,8 +9,18 @@ import * as cors from 'cors';
 import * as helmet from 'helmet';
 import { Logger } from '../logger/logger';
 import { CustomError, ErrorType, InternalError, NotFoundError } from '../errors/errors';
-import { Router } from 'express';
+import { Request, Router } from 'express';
 import * as bodyParser from 'body-parser';
+import { v4 } from 'uuid';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      id?: string;
+    }
+  }
+}
 
 export interface CorsOptions {
   origin: string;
@@ -32,36 +42,50 @@ export class HttpServer {
     this.logger.debug('Health check');
   }
 
-  setupDocs(docsPath: string) {
+  private log(req: Request, msg?: string): string {
+    return `${req.method}(${req.id}) ${req.baseUrl}${req.path}: ${msg}`;
+  }
+
+  setupDocs(docsPath: string): void {
     const docsPaths = glob.sync(`${docsPath}/*.yaml`);
     docsPaths.forEach((docPath) => {
       const swaggerDocument = yaml.load(docPath);
       const name = path.basename(docPath).replace('.yaml', '');
       const url = `/docs/${name}`;
       this.app.use(url, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-      this.logger.info(`Loaded documentation "${name}" ${this.baseUrl}:${this.port}${url}/`);
+      this.logger.info(`Loaded documentation page "${name}" ${this.baseUrl}:${this.port}${url}/`);
     });
   }
 
-  setupCors(opts?: CorsOptions) {
+  setupCors(opts?: CorsOptions): void {
     this.logger.debug('Setting up cors');
     this.app.use(cors(opts));
   }
 
-  setupMiddleware() {
+  loadMiddlewares(): void {
     this.logger.debug('Setting up middlewares');
     this.app.use(helmet());
     this.app.use(bodyParser.json());
+    this.app.use((req, res, next) => {
+      req.id = v4();
+      next();
+    });
+    this.app.use((req, res, next) => {
+      res.on('finish', () => {
+        this.logger.http(this.log(req, 'status ' + res.statusCode.toString()));
+      });
+      next();
+    });
   }
 
-  loadRoutes(routes: { prefix: string; router: Router }[]) {
+  loadRoutes(routes: { prefix: string; router: Router }[]): void {
     routes.forEach((route) => {
       this.logger.debug(`Loading router /${route.prefix}`);
       this.app.use('/' + route.prefix, route.router);
     });
   }
 
-  start() {
+  start(): void {
     this.app.use('**', (req, res, next) => next(new NotFoundError("Page doesn't exist")));
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -70,13 +94,13 @@ export class HttpServer {
       const error: CustomError = !!err.type ? err : new InternalError(err);
       switch (error.type) {
         case ErrorType.IncorrectInput:
-          this.logger.debug(`(${req.method} ${req.path}) ${error.errorDetails}`);
+          this.logger.debug(this.log(req, error.errorDetails));
           return res.status(400).json({ error: error.type, details: error.errorDetails });
         case ErrorType.NotFound:
-          this.logger.debug(`(${req.method} ${req.path}) ${error.errorDetails}`);
+          this.logger.debug(this.log(req, error.errorDetails));
           return res.status(404).json({ error: error.type, details: error.errorDetails });
         default:
-          this.logger.error(`(${req.method} ${req.path}) ${error.stack || error.errorDetails}`);
+          this.logger.error(this.log(req, error.errorDetails || error.stack));
           return res.status(500).json({ error: error.type });
       }
     });
